0b759266961e546eb42d9b8008a4ca4cb5a9755f
Sajith Sajan Koshy <sajith.koshy@volkswagen.co.in>
2024-08-25 17:45:38 +0530

    Removed unwanted method

--- src/main/java/org/example/usercrud/Model/User.java
+++ src/main/java/org/example/usercrud/Model/User.java
@@ -20,14 +20,6 @@ public class User {
         this.username = username;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getPassword() {
         return password;
     }
